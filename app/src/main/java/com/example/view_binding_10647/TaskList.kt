e0f6af34bcda661604e004a23fd0ca87d6f22491
ValentinoDevinS <valentinodevins11@gmail.com>
2022-09-15 08:40:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10647/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10647
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter1", "Bangun Tidur", "Kebiasaan Sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari Primo buat Gacha Mama Yaemiko tercinta / keqing wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mama Ei?", "Buat Adu Mekanik Showcase Ayaka Pyro"),
+        Task("Chapter 5", "Upload ke Tiktok", "Buat pamer ke view Showcase Ayaka Pyro C6 + Bannet C6")
+    )
+
+}
